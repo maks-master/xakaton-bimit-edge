2af88626eb9cd1244b609bd0d24a63eee5210ace
Master Maks <max@bimit.ru>
2021-05-21 19:51:58 +0400

    dddddd

---
+++ src/main/java/ru/xakaton/bimit/XakatonBimitEdgeApplication.java
@@ -0,0 +1,13 @@
+package ru.xakaton.bimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XakatonBimitEdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XakatonBimitEdgeApplication.class, args);
+	}
+
+}
