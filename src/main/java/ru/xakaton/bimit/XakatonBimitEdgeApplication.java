81fa8d3274b9328abe49702ee9492ff2d18fdea1
Master Maks <master@mifors.com>
2021-05-22 09:53:27 +0400

    Болванка старт

--- src/main/java/ru/xakaton/bimit/XakatonBimitEdgeApplication.java
+++ src/main/java/ru/xakaton/bimit/XakatonBimitEdgeApplication.java
@@ -2,12 +2,20 @@ package ru.xakaton.bimit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class XakatonBimitEdgeApplication {
+public class XakatonBimitEdgeApplication extends SpringBootServletInitializer {
 
+	// for war
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(XakatonBimitEdgeApplication.class);
+	}
+
+	// for jar
 	public static void main(String[] args) {
 		SpringApplication.run(XakatonBimitEdgeApplication.class, args);
 	}
-
 }
